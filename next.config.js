5c68b1599e61194a125424bfd1537589655f4364
kuldip <kuldiplohare80@gmail.com>
2025-06-19 23:08:52 +0530

    fix: remove static export for dynamic deployment

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  reactStrictMode: true,
+  // Do not use output: 'export'
 };
 
 module.exports = nextConfig;
